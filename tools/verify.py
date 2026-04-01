50293ab52b73e2b4b1066d88f6583b589a89d9f8
FlemingH <lm.fei@outlook.com>
2026-04-01 20:26:32 +0800

    Update hip_model loading to allow non-strict state dict matching

--- tools/verify.py
+++ tools/verify.py
@@ -103,7 +103,7 @@ def main():
 
     torch_model = Model(*init_inputs).eval().cuda()
     hip_model = ModelNew(*init_inputs).eval().cuda()
-    hip_model.load_state_dict(torch_model.state_dict())
+    hip_model.load_state_dict(torch_model.state_dict(), strict=False)
 
     num_checks = 5
     pass_count = 0
