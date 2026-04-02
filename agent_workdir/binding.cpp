a0654ddeb84bb90544c4745d8ca33ad8b5f2c589
FlemingH <lm.fei@outlook.com>
2026-04-02 17:43:05 +0800

    refactor: change module name to use TORCH_EXTENSION_NAME for better integration

--- agent_workdir/binding.cpp
+++ agent_workdir/binding.cpp
@@ -1,6 +1,6 @@
 #include <pybind11/pybind11.h>
 #include "binding_registry.h"
 
-PYBIND11_MODULE(hip_extension, m) {
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
     BindingRegistry::getInstance().applyBindings(m);
 }
